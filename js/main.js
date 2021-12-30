8776c2eab151530084e07a66a105177c42b5ab47
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-12-30 13:06:44 +0900

    apply changes for js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
 var slides = document.querySelector('.slider').children;
 var nextSlide = document.querySelector('.slider-btn-left');
 var prevSlide = document.querySelector('.slider-btn-right');
+var index = 0;
 
 var totalSlides = slides.length;
 
 nextSlide.onclick = function(){
-    console.log("click");
+    next("next");
 }
+prevSlide.onclick = function(){
+    next("prev");
+}
+
+// function next(direction){
+//     if(direction == "next"){
+//         index++;
+//         if(index==totalSlides){
+//             index = 0;
+//         }
+//     }
+
+//     for(i=0; i<totalSlides.length; i++){
+//         slides[i].classList.remove("active");
+//     }
+//     slides[index].classList.add("passive");
+// }
